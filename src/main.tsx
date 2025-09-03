7c45335a6d7be55ff92edc60fe3b3ee5eb02c41d
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-09-03 11:11:32 -0300

    feat: creating first routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createRootRoute, createRoute, createRouter, RouterProvider } from "@tanstack/react-router";
+
 import "./assets/css/Index.css";
 import App from "./App.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+const rootRoute = createRootRoute({
+  notFoundComponent: NotFound,
+});
+
+const routes = [
+  createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/",
+    component: App,
+  }),
+];
+
+const routeTree = rootRoute.addChildren(routes);
+const router = createRouter({ routeTree });
+
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,31 @@
+import Accessibility from "../components/Accessibility";
+
+import NotFound from "../assets/svgs/not-found.svg";
+
+export default () => {
+  return (
+    <>
+      <Accessibility />
+      <header
+        className="
+      relative 
+      header-main 
+      flex 
+      flex-col 
+      items-center justify-center font-bold text-white gap-5 sm:flex-row"
+      >
+        <img src="/icon.png" className="icon-formater" alt="logotipo-camara" />
+        <aside className="text-xl uppercase text-center">
+          <h1 className="text-2xl xl:text-5xl font-black">Câmara municipal de vereadores</h1>
+          <h2 className="xl:text-3xl">casa pedro cesar de carvalho</h2>
+          <h3 className="font-normal">IATI - pe</h3>
+        </aside>
+      </header>
+      <section className="h-100 flex items-center flex-col justify-center font-bold text-xl">
+        <img src={NotFound} width={350} alt="pagina-não-encontrada-img" />
+        <h2>404</h2>
+        <h1>PAGINA NÃO ENCONTRADA</h1>
+      </section>
+    </>
+  );
+};
