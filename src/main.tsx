e7f7a0c84ceb8cc8593e3d1b809c2362cb1bd365
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-10-06 09:07:41 -0300

    feat: add area to a specific counciliors

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import App from "./App";
 import Municipio from "./pages/landing/Municipio";
 import Camara from "./pages/landing/Camara";
 import CouncilorsPage from "./pages/landing/CouncilorsPage";
+import HomeConcilior from "./pages/personal/HomeConcilior";
 
 const rootRoute = createRootRoute();
 
@@ -17,6 +18,11 @@ const routes = [
     path: "/",
     component: App,
   }),
+  /*createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/vereador/:councilorId",
+    component: HomeConcilior,
+  }),*/
   createRoute({
     getParentRoute: () => rootRoute,
     path: "/vereadores",
