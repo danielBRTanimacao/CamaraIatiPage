2782431e3a36c6affe852ae51bfbd922c2fcbe33
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-01-25 11:40:53 -0300

    feat: add header details and more efficient code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,50 @@
 import { Link } from "@tanstack/react-router";
-import IconMunicipio from "../assets/svgs/icone-municipio.svg";
 import { useState } from "react";
+import {
+    BsList,
+    BsHouseGearFill,
+    BsPaperclip,
+    BsPersonRolodex,
+    BsPeople,
+    BsArchive,
+    BsHeadset,
+} from "react-icons/bs";
+
+import IconMunicipio from "../assets/svgs/icone-municipio.svg";
 
 export default () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const pageLegislation =
         "http://162.214.186.142:16444/transparencia/iati/camara/legislacaomunicipal.faces";
 
+    const CustomLink = ({ item, children }: { item: any; children: React.ReactNode }) => {
+        const isExternal = item.href.startsWith("http");
+        const className =
+            "transition-all hover:scale-110 p-4 w-36 h-36 sm:w-40 bg-green-700 hover:bg-green-800 rounded-lg flex flex-col items-center justify-center text-center";
+
+        if (isExternal) {
+            return (
+                <a href={item.href} className={className} target="_blank" rel="noopener noreferrer">
+                    {children}
+                </a>
+            );
+        }
+        return (
+            <Link to={item.href} className={className}>
+                {children}
+            </Link>
+        );
+    };
+
     return (
         <>
-            <header
-                className="
-      relative 
-      header-main 
-      flex 
-      flex-col 
-      items-center justify-center font-bold text-white gap-5 sm:flex-row"
-            >
+            <header className="relative header-main flex flex-col items-center justify-center font-bold text-white gap-5 sm:flex-row">
                 <button
                     id="burger"
                     onClick={() => setMenuOpen(!menuOpen)}
-                    className="cursor-pointer 
-          absolute bottom-10 
-          left-1/25 -translate-x-1/2 top-1 px-3
-          rounded-lg transition-colors border 
-          border-white/50 opacity-75 shadow-lg sm:hidden"
+                    className="cursor-pointer absolute bottom-10 left-1/25 -translate-x-1/2 top-1 px-3 rounded-lg transition-colors border border-white/50 opacity-75 shadow-lg sm:hidden"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="32"
-                        height="32"
-                        fill="currentColor"
-                        className="bi bi-list"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                        />
-                    </svg>
+                    <BsList size={32} />
                 </button>
 
                 <img src="/icon.png" className="icon-formater" alt="logotipo-camara" />
@@ -52,12 +58,7 @@ export default () => {
             </header>
 
             {menuOpen && (
-                <aside
-                    className="fixed 
-        top-0 right-0 h-screen w-3/4 
-        bg-amber-400 text-white shadow-lg 
-        z-50 flex flex-col items-end gap-5 p-5 sm:hidden"
-                >
+                <aside className="fixed top-0 right-0 h-screen w-3/4 bg-amber-400 text-white shadow-lg z-50 flex flex-col items-end gap-5 p-5 sm:hidden">
                     <button
                         onClick={() => setMenuOpen(false)}
                         className="self-end text-2xl font-bold cursor-pointer transition-all hover:scale-125"
@@ -76,9 +77,9 @@ export default () => {
                             </Link>
                         </li>
                         <li>
-                            <Link to={pageLegislation} className="transition-all hover:text-xl">
+                            <a href={pageLegislation} className="transition-all hover:text-xl">
                                 Legislação
-                            </Link>
+                            </a>
                         </li>
                         <li>
                             <Link to="/vereadores" className="transition-all hover:text-xl">
@@ -86,28 +87,17 @@ export default () => {
                             </Link>
                         </li>
                         <li>
-                            <Link
-                                to="http://162.214.186.142:16444/transparencia/iati/camara/"
-                                className="transition-all hover:text-xl"
-                            >
-                                Transparência pública
-                            </Link>
+                            <a href="http://162.214.186.142:16444/transparencia/iati/camara/">
+                                Transparência
+                            </a>
                         </li>
                         <li>
-                            <Link
-                                to="http://162.214.186.142:16444/transparencia/iati/camara/esic.faces"
+                            <a
+                                href="http://162.214.186.142:16444/transparencia/iati/camara/esic.faces"
                                 className="transition-all hover:text-xl"
                             >
                                 e-SIC
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="http://162.214.186.142:16444/transparencia/iati/camara/esic.faces"
-                                className="transition-all hover:text-xl"
-                            >
-                                Ouvidoria
-                            </Link>
+                            </a>
                         </li>
                     </ul>
                 </aside>
@@ -119,124 +109,48 @@ export default () => {
                         {
                             href: "/municipio",
                             label: "Municipio",
+                            image: <img src={IconMunicipio} alt="" />,
                             extra: "hidden md:block",
-                            image: IconMunicipio,
                         },
                         {
                             href: "/camara",
                             label: "A Câmara",
-                            image: (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40"
-                                    height="40"
-                                    fill="currentColor"
-                                    className="bi bi-house-gear-fill"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708z" />
-                                    <path d="M11.07 9.047a1.5 1.5 0 0 0-1.742.26l-.02.021a1.5 1.5 0 0 0-.261 1.742 1.5 1.5 0 0 0 0 2.86 1.5 1.5 0 0 0-.12 1.07H3.5A1.5 1.5 0 0 1 2 13.5V9.293l6-6 4.724 4.724a1.5 1.5 0 0 0-1.654 1.03" />
-                                    <path d="m13.158 9.608-.043-.148c-.181-.613-1.049-.613-1.23 0l-.043.148a.64.64 0 0 1-.921.382l-.136-.074c-.561-.306-1.175.308-.87.869l.075.136a.64.64 0 0 1-.382.92l-.148.045c-.613.18-.613 1.048 0 1.229l.148.043a.64.64 0 0 1 .382.921l-.074.136c-.306.561.308 1.175.869.87l.136-.075a.64.64 0 0 1 .92.382l.045.149c.18.612 1.048.612 1.229 0l.043-.15a.64.64 0 0 1 .921-.38l.136.074c.561.305 1.175-.309.87-.87l-.075-.136a.64.64 0 0 1 .382-.92l.149-.044c.612-.181.612-1.049 0-1.23l-.15-.043a.64.64 0 0 1-.38-.921l.074-.136c.305-.561-.309-1.175-.87-.87l-.136.075a.64.64 0 0 1-.92-.382ZM12.5 14a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
-                                </svg>
-                            ),
+                            image: <BsHouseGearFill size={40} />,
                         },
                         {
                             href: pageLegislation,
                             label: "Legislação",
-                            image: (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40"
-                                    height="40"
-                                    fill="currentColor"
-                                    className="bi bi-paperclip"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z" />
-                                </svg>
-                            ),
+                            image: <BsPaperclip size={40} />,
                         },
                         {
                             href: "/vereadores",
                             label: "Vereadores",
+                            image: <BsPersonRolodex size={40} />,
                             extra: "hidden lg:block",
-                            image: (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40"
-                                    height="40"
-                                    fill="currentColor"
-                                    className="bi bi-person-rolodex"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
-                                    <path d="M1 1a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h.5a.5.5 0 0 0 .5-.5.5.5 0 0 1 1 0 .5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5.5.5 0 0 1 1 0 .5.5 0 0 0 .5.5h.5a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H6.707L6 1.293A1 1 0 0 0 5.293 1zm0 1h4.293L6 2.707A1 1 0 0 0 6.707 3H15v10h-.085a1.5 1.5 0 0 0-2.4-.63C11.885 11.223 10.554 10 8 10c-2.555 0-3.886 1.224-4.514 2.37a1.5 1.5 0 0 0-2.4.63H1z" />
-                                </svg>
-                            ),
                         },
                         {
                             href: "http://162.214.186.142:16444/transparencia/iati/camara/",
-                            label: "Transparência pública",
+                            label: "Transparência",
+                            image: <BsPeople size={40} />,
                             extra: "hidden 2xl:block",
-                            image: (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40"
-                                    height="40"
-                                    fill="currentColor"
-                                    className="bi bi-people"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-                                </svg>
-                            ),
                         },
                         {
                             href: "http://162.214.186.142:16444/transparencia/iati/camara/esic.faces",
                             label: "e-SIC",
-                            image: (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40"
-                                    height="40"
-                                    fill="currentColor"
-                                    className="bi bi-archive"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
-                                </svg>
-                            ),
+                            image: <BsArchive size={40} />,
                         },
                         {
                             href: "http://162.214.186.142:16444/transparencia/iati/camara/esic.faces",
                             label: "Ouvidoria",
+                            image: <BsHeadset size={40} />,
                             extra: "hidden xl:block",
-                            image: (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40"
-                                    height="40"
-                                    fill="currentColor"
-                                    className="bi bi-headset"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5" />
-                                </svg>
-                            ),
                         },
-                    ].map((item) => (
-                        <li key={item.href} className={item.extra || ""}>
-                            <a
-                                className="transition-all hover:scale-110 p-4 w-36 h-36 sm:w-40 bg-green-700 hover:bg-green-800 rounded-lg flex flex-col items-center justify-center text-center"
-                                href={item.href}
-                            >
-                                {typeof item.image == "string" ? (
-                                    <img src={item.image} alt="" />
-                                ) : (
-                                    item.image
-                                )}
+                    ].map((item, idx) => (
+                        <li key={idx} className={item.extra || ""}>
+                            <CustomLink item={item}>
+                                {item.image}
                                 <span className="mt-2">{item.label}</span>
-                            </a>
+                            </CustomLink>
                         </li>
                     ))}
                 </ul>
